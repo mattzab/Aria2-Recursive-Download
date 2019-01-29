0c898da2546009cd741bd283ee22f6bae8e049de
mattzab <mattzab@gmail.com>
2019-01-29 14:35:39 -0600

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import os
 
 def download():
-	os.system("aria2c --file-allocation=none -c --auto-file-renaming=false -i links.txt")
+	os.system("aria2c --optimize-concurrent-downloads true -j 5000 -x 5 --file-allocation=none -c --auto-file-renaming=false -i links.txt")
 
 folders = [ x[0] for x in os.walk(os.getcwd()) ]
 folders.sort()
